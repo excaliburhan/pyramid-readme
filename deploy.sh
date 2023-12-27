a94575502b99108d1869152f66458658c9090ca8
xiaoping <edwardhjp@gmail.com>
2023-12-27 13:20:25 +0800

    feat: 🎸 update

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:excaliburhan/pyramid-readme.git master:gh-pages
+git push -f git@github.com:excaliburhan/pyramid-readme.git main:gh-pages
 
 cd -
